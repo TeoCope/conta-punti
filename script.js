ec6279110ddccf20e0618f331bdc101ba4ed1a25
Matteo Copertari <s1088230@studenti.univpm.it>
2023-07-12 14:44:40 +0200

    terzo commit

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let bottoneIndietro = document.getElementById('indietro');
+const home = document.getElementById('home');
+const formaSquadre = document.getElementById('formFormaSquadre');
 
-bottoneIndietro.addEventListener('click',function(){
-    history.back();
+var bottoneGioca = document.getElementById('gioca');
+
+bottoneGioca.addEventListener('click',function() {
+    displayNone(home);
+    displayFlex(formaSquadre);
 });
+
+function displayNone(elemento){
+    elemento.style.display="none";
+}
+
+function displayFlex(elemento){
+    elemento.style.display="flex";
+}
+
